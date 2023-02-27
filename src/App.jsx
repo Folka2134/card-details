82581349908476f245103c0d5acc4a108ad4027c
Folka2134 <folka2134@gmail.com>
2023-02-27 06:46:21 +0000

    finished cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,29 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>test</div>
+      <div className="h-80 bg-[#21082F] p-4">
+        <div className="card-front h-56 w-96 p-10 rounded-lg bg-gradient-to-br from-[#6142F5] via-[#A353D0] to-[#650792] text-white absolute inset-y-36 inset-x-5 z-10">
+          <div className="flex flex-col h-full justify-between pb-4">
+            <h1></h1>
+            <h1 className="text-center text-3xl tracking-widest">
+              0000000000000000
+            </h1>
+          </div>
+          <div className="flex justify-between text-sm">
+            <h3 className="uppercase">Jane Appleaseed</h3>
+            <p>00/00</p>
+          </div>
+        </div>
+        <div className="card-front h-56 w-96 pt-4 rounded-lg bg-gray-100 right-6 absolute">
+          <h1 className="h-10 bg-black">.</h1>
+          <div className="p-5">
+            <div className="text-right text-white tracking-widest rounded-md p-2 bg-gray-400">
+              000
+            </div>
+          </div>
+        </div>
+      </div>
+      <main className="">text</main>
     </div>
   );
 }
