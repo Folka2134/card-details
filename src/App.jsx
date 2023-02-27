50e3f3c4a5f58e2f8a2d583b3efd899520e03aa7
Folka2134 <folka2134@gmail.com>
2023-02-27 08:43:17 +0000

    implemented form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,49 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 import "./App.css";
 
 function App() {
+  // Formik Logic
+
+  const formik = useFormik({
+    initialValues: {
+      cardholderName: "",
+      cardNumber: "",
+      mm: "",
+      yy: "",
+      cvc: "",
+    },
+
+    // Validate Form
+
+    validationSchema: Yup.object({
+      cardholderName: Yup.string().required("Can't be blank"),
+      cardNumber: Yup.number()
+        .min(16, "must have atleast 16 numbers")
+        .typeError("wrong fomat, numbers only")
+        .required("Can't be blank"),
+      mm: Yup.number()
+        .min(2, "requires 2 digits")
+        .typeError("wrong fomat, numbers only")
+        .required("Can't be blank"),
+      yy: Yup.number()
+        .min(2, "requires 2 digits")
+        .typeError("wrong fomat, numbers only")
+        .required("Can't be blank"),
+      cvc: Yup.number()
+        .min(3, "requires 3 digits")
+        .typeError("wrong fomat, numbers only")
+        .required("Can't be blank"),
+    }),
+
+    // Submit Form
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  // console.log(formik.errors);
   return (
     <div className="App">
       <div className="h-80 bg-[#21082F] p-4">
@@ -26,10 +69,17 @@ function App() {
         </div>
       </div>
       <main className="p-10 mt-12">
-        <form className="flex flex-col justify-evenly h-96">
+        <form
+          className="flex flex-col justify-evenly h-96"
+          onSubmit={formik.handleSubmit}
+        >
           <div>
             <label
-              className="text-sm font-semibold tracking-wider uppercase"
+              className={`text-sm font-semibold tracking-wider uppercase ${
+                formik.errors.cardholderName && formik.touched.cardholderName
+                  ? "border-red-400"
+                  : "border-gray-200"
+              }`}
               htmlFor="cardName"
             >
               Cardholder Name
@@ -37,62 +87,121 @@ function App() {
             <input
               className="w-full p-2 border-2 border-gray-300 mt-2"
               type="text"
+              name="cardholderName"
               placeholder="e.g Jane Appleseed"
+              value={formik.values.cardholderName}
+              onChange={formik.handleChange}
             />
+            {formik.errors.cardholderName && formik.touched.cardholderName && (
+              <label className="text-xs text-red-400 p-2 text-right italic">
+                {formik.errors.cardholderName}
+              </label>
+            )}
           </div>
           <div>
             <label
-              className="text-sm font-semibold tracking-wider uppercase"
-              htmlFor="cardNumber"
+              className={`text-sm font-semibold tracking-wider uppercase ${
+                formik.errors.cardNumber && formik.touched.cardNumber
+                  ? "border-red-400"
+                  : "border-gray-200"
+              }`}
             >
               Card Number
             </label>
             <input
               className="w-full p-2 border-2 border-gray-300 mt-2"
               type="text"
+              name="cardNumber"
               placeholder="e.g 1234 5678 9123 0000"
+              value={formik.values.cardNumber}
+              onChange={formik.handleChange}
             />
+            {formik.errors.cardNumber && formik.touched.cardNumber && (
+              <label className="text-xs text-red-400 p-2 text-right italic">
+                {formik.errors.cardNumber}
+              </label>
+            )}
           </div>
           <div className="flex justify-between">
             <div className="flex flex-col">
               <label
-                className="text-sm font-semibold tracking-wider uppercase"
-                htmlFor="cardNumber"
+                className={`text-sm font-semibold tracking-wider uppercase ${
+                  (formik.errors.mm && formik.touched.mm) ||
+                  (formik.errors.yy && formik.touched.yy)
+                    ? "border-red-400"
+                    : "border-gray-200"
+                }`}
               >
                 Exp. Date (mm/yy)
               </label>
               <div className="flex">
-                <input
-                  className="w-14 p-2 border-2 border-gray-300 mr-3 mt-2"
-                  type="text"
-                  placeholder="MM"
-                />
-                <input
-                  className="w-14 p-2 border-2 border-gray-300 mr-3 mt-2"
-                  type="text"
-                  placeholder="YY"
-                />
+                <div className="flex flex-col">
+                  <input
+                    className="w-14 p-2 border-2 border-gray-300 mr-3 mt-2"
+                    type="text"
+                    name="mm"
+                    placeholder="MM"
+                    maxLength={2}
+                    value={formik.values.mm}
+                    onChange={formik.handleChange}
+                  />
+                  {formik.errors.mm && formik.touched.mm && (
+                    <label className="text-xs text-red-400 p-2 text-right italic">
+                      {formik.errors.mm}
+                    </label>
+                  )}
+                </div>
+                <div className="flex flex-col">
+                  <input
+                    className="w-14 p-2 border-2 border-gray-300 mr-3 mt-2"
+                    type="text"
+                    name="yy"
+                    placeholder="YY"
+                    maxLength={2}
+                    value={formik.values.yy}
+                    onChange={formik.handleChange}
+                  />
+                  {formik.errors.yy && formik.touched.yy && (
+                    <label className="text-xs text-red-400 p-2 text-right italic">
+                      {formik.errors.yy}
+                    </label>
+                  )}
+                </div>
               </div>
             </div>
             <div className="flex flex-col">
               <label
-                className="text-sm font-semibold tracking-wider uppercase"
-                htmlFor="cardNumber"
+                className={`text-sm font-semibold tracking-wider uppercase ${
+                  formik.errors.cvc && formik.touched.cvc
+                    ? "border-red-400"
+                    : "border-gray-200"
+                }`}
               >
                 CVC
               </label>
               <input
                 className="w-28 p-2 border-2 border-gray-300 mr-3 mt-2"
                 type="text"
+                name="cvc"
                 placeholder="e.g 123"
+                maxLength={3}
+                value={formik.values.cvc}
+                onChange={formik.handleChange}
               />
+              {formik.errors.cvc && formik.touched.cvc && (
+                <label className="text-xs text-red-400 p-2 text-right italic">
+                  {formik.errors.cvc}
+                </label>
+              )}
             </div>
           </div>
-          <input
+          <button
             className="bg-[#21082F] text-white p-4 rounded-xl"
             type="submit"
             value="Confirm"
-          />
+          >
+            Confirm
+          </button>
         </form>
       </main>
     </div>
